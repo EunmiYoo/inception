5a59440a4bd5f300ac40b3543b86ad174caf6051
eyoo <navi0286@gmail.com>
2022-12-05 05:20:52 +0100

    mariadb root password modification

--- scrs/requirements/mariadb/tools/run.sh
+++ scrs/requirements/mariadb/tools/run.sh
@@ -6,8 +6,9 @@ echo "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME" | mysql -u root
 echo "CREATE USER IF NOT EXISTS '$DATABASE_LOGIN'@'%'"| mysql -u root
 echo "GRANT ALL PRIVILEGES ON *.* TO '$DATABASE_LOGIN'@'%' IDENTIFIED BY '$DATABASE_PASSWORD'"| mysql -u root
 
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PASSWORD'" | mysql -u root 
 echo "FLUSH PRIVILEGES" | mysql -u root
 
-service mysql stop
+mysqladmin -u root -p$ROOT_PASSWORD shutdown
 
-exec /usr/sbin/mysqld -u root
+exec /usr/bin/mysqld_safe
